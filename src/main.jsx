16bb76831fa18a691ebff4817bd38bb5b5c3cb6b
shoutingminds-GY <shoutingminds@gmail.com>
2026-03-07 01:40:29 +0530

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TradingPlatform from './TradingPlatform.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TradingPlatform />
-  </React.StrictMode>
-)
